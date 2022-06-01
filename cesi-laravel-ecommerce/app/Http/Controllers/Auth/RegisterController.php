909e2d880c00c411bcdecd95776eaf7c6797a737
MDANDEL <maxime.dandel@suez.com>
2022-06-01 13:44:23 +0200

    minor

--- cesi-laravel-ecommerce/app/Http/Controllers/Auth/RegisterController.php
+++ cesi-laravel-ecommerce/app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
@@ -29,8 +30,13 @@ class RegisterController extends Controller
         ]);
 
         $user = User::create([
+            'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
 }
--- cesi-laravel-ecommerce/resources/views/auth/register.blade.php
+++ cesi-laravel-ecommerce/resources/views/auth/register.blade.php
@@ -7,12 +7,23 @@
             <div class="card">
                 <div class="card-body">
                     <h1>Inscription</h1>
-                    <form action="{{ route('login') }}" method="POST">
+                    <form action="{{ route('register') }}" method="POST">
                         @csrf
+
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="name" class="form-control @error('name') is-invalid @enderror" id="name " name="name" value="{{ old('name') }}">
+                            @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
                         <div class="mb-3">
                             <label for="email" class="form-label">Email address</label>
                             <input type="email" class="form-control @error('email') is-invalid @enderror" id="email " name="email" value="{{ old('email') }}">
-                            @error('name')
+                            @error('email')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -22,7 +33,7 @@
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" value="{{ 'password' }}">
-                            @error('name')
+                            @error('password')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
@@ -31,7 +42,7 @@
 
                         <div class="mb-3">
                             <label for="password_confirmation" class="form-label">Password Confirmation</label>
-                            <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" name="password_confirmation" value="{{ 'password' }}">
+                            <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" name="password_confirmation" value="{{ 'password_confirmation' }}">
                             @error('password_confirmation')
                             <div class="invalid-feedback">
                                 {{ $message }}
